6ad4a86d876bfabd3e4d9d69c5330ea7bda3dcca
James Okunola <kingjamesegun@gmail.com>
2020-11-30 04:26:48 +0100

    Updates

---
+++ precache-manifest.9b212a7f53823c64df719133b68c419f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758ac1cf55feeef1dc2ee5485cfe4030",
+    "url": "/YouTubeClone/index.html"
+  },
+  {
+    "revision": "10ce1ea0409698a27185",
+    "url": "/YouTubeClone/static/css/main.7014b8ca.chunk.css"
+  },
+  {
+    "revision": "3a2cd37fada25a7f7f5c",
+    "url": "/YouTubeClone/static/js/2.eabc9141.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/YouTubeClone/static/js/2.eabc9141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10ce1ea0409698a27185",
+    "url": "/YouTubeClone/static/js/main.94e8a241.chunk.js"
+  },
+  {
+    "revision": "791aa2dae6456c4d54e0",
+    "url": "/YouTubeClone/static/js/runtime-main.357df80f.js"
+  }
+]);
